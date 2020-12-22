44787d90a9d2afdde81ae31334d8e96ef1dd3006
Esemega <sogaral89@gmail.com>
2020-12-22 21:42:02 +0100

    Getting movements of a selectedId from api, instead of getting all list and filtering it

--- src/pages/movements/movements.api.js
+++ src/pages/movements/movements.api.js
@@ -3,12 +3,10 @@ import Axios from 'axios';
 const urlMovements = `${process.env.BASE_API_URL}/movements`;
 const urlAccount = `${process.env.BASE_API_URL}/account`;
 
-export const getMovements = () =>
-  Axios.get(`${urlMovements}`)
-  .then((response) => response.data)
-  .catch(error => console.log(error));
+export const getMovements = (id) =>
+  Axios.get(urlMovements, { params: { accountId: id } }).then(
+    (response) => response.data
+  );
 
 export const getAccount = (id) =>
-  Axios.get(`${urlAccount}/${id}`)
-  .then((response) => response.data)
-  .catch(error => console.log(error));
+  Axios.get(`${urlAccount}/${id}`).then((response) => response.data);
